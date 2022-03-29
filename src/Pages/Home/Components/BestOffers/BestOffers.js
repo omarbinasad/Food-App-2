bb41f3b4de958829f2340593538b1829b9b23ba3
unknown <mrafi5268@gmail.com>
2022-03-29 23:10:59 +0600

    some changes

--- src/Pages/Home/Components/BestOffers/BestOffers.js
+++ src/Pages/Home/Components/BestOffers/BestOffers.js
@@ -8,11 +8,21 @@ const BestOffers = () => {
         <h3 className="bestOffers-title text-start or-divider">Best Offers</h3>
         <a href="/">See all</a>
       </div>
-      <div className="bestOffers-div">
-        <div className="active">
-          <div></div>
-          <div></div>
-          <div></div>
+      <div className="bestOffers-div-main">
+        <div className="bestOffer-div">
+          <div className=" bestOffers-img-div">
+            <img src="/images/bestoffer.png" alt="" />
+          </div>
+          <div className=" bestOffers-text-div">
+            <p>
+              Get Fresh <br /> Start At 50/- <br /> only
+            </p>
+          </div>
+          <div className=" bestOffers-btn-div">
+            <button className="bestOffers-btn">
+              <i class="fa-solid fa-arrow-right-long"></i>
+            </button>
+          </div>
         </div>
         <div></div>
       </div>
